93c7aaaa91fba18b715d6b2f25baf8ac8eba891a
Tobias Zwink <tobiaszwink@TobiasZwink.local>
2015-06-29 17:19:17 -0300

    adds css to header

---
+++ header.php
@@ -0,0 +1,13 @@
+<header id="header">
+  <div class="container-h">
+    <h1>HangApp</h1>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+        <li>Quem somos</li>
+        <li>Parceiros</li>
+      </ul>
+    </nav>
+  </div>
+</header>
--- index.php
+++ index.php
@@ -3,10 +3,17 @@
 <head>
   <meta charset="utf-8">
   <title>HangApp</title>
+  <link rel="stylesheet" type="text/css" href="css/application.css">
+  <script src="js/application.js"></script>
 </head>
 <body>
-<?php
-  include 'header.php';
-?>
+  <?php
+    include 'header.php';
+  ?>
+  <div class="wrapper">
+    <div class="container">
+      <p>Section</p>
+    </div>
+  </div>
 </body>
 </html>
